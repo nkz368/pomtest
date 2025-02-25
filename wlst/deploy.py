034b06bb6e1035670bf22f8d71109f23f13907d8
nakajima-ke <nakajima-ke@ncontr.com>
2025-02-25 16:26:19 +0900

    commit

--- wlst/deploy.py
+++ wlst/deploy.py
@@ -1,3 +1,5 @@
 connect('weblogic', 'wls12345', 't3://155.248.161.146:7001')
-deploy('pomtest', '/tmp/pomtest/target/pomtest', targets='AdminServer')
+deploy('pomtest', '/tmp/pomtest/target/pomtest.war', targets='AdminServer')
 
+disconnect()
+exit()
